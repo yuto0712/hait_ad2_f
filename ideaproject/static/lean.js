32dcdac91cb220df4f495fefd4f1e649754cd4cb
love-rabbit <parprin1118@icloud.com>
2020-12-13 12:07:06 +0900

    leancanvas機能完成

--- ideaproject/static/lean.js
+++ ideaproject/static/lean.js
@@ -19,13 +19,13 @@ function loadImage(id)
 }
 
 //キャンバスに文字を描く
-function drawText(canvas_id, text_id, x, y)
+function drawText(canvas_id, text_id, x, y, len)
 {
   var canvas = document.getElementById(canvas_id);
   var ctx = canvas.getContext('2d');
   var text = document.getElementById(text_id);
   //文字のスタイルを指定
-  ctx.font = '13px serif';
+  ctx.font = '13px serif'; 
   ctx.fillStyle = 'black';
   //文字の配置を指定（左上基準にしたければtop/leftだが、文字の中心座標を指定するのでcenter
   ctx.textBaseline = 'top';
@@ -33,6 +33,73 @@ function drawText(canvas_id, text_id, x, y)
   //座標を指定して文字を描く
   var x = x;
   var y = y;
-  ctx.fillText(text.value, x, y);
+  //何文字で改行するか指定
+  var len = len;
+  //入力文字を1文字毎に配列化
+	var aryText = text.value.split('');
+	//出力用の配列を用意
+	var aryRow = [];
+	aryRow[0] = '';
+	var row_cnt = 0;
+	//入力1文字毎にループ　改行コードもしくは折り返しで配列の添え字を足す
+	for(var i = 0; i < aryText.length; i++){
+		var ary_text = aryText[i];
+    if(aryRow[row_cnt].length >= len){
+			row_cnt++;
+			aryRow[row_cnt] = '';
+		}
+		if(ary_text == "\n"){
+			row_cnt++;
+			aryRow[row_cnt] = '';
+			ary_text = '';
+		}
+		aryRow[row_cnt] += ary_text;
+	}
+  //文字の表示　y軸とx軸をループする
+  var font_size = 13
+	for(var i = 0; i < aryRow.length; i++){
+		aryStr = aryRow[i].split('');
+		for(var j = 0; j < aryStr.length; j++){
+			ctx.fillText(aryStr[j], x + (j * font_size), y + (i * font_size));
+		}
+  }
+  alert('リーンキャンバスに内容を書き込みました。');
 }
-
+//リセット機能
+function resetText(canvas_id, text_id, x, y, width, height) {
+  var canvas = document.getElementById(canvas_id);
+  var ctx = canvas.getContext('2d');
+  //座標と範囲を決める
+  var x = x;
+  var y = y;
+  var width = width;
+  var height = height;
+  //キャンバスの内容を白で塗りつぶす
+  ctx.fillStyle = "white";
+  ctx.fillRect(x, y, width, height);
+  //textarea内の文字を消去
+  var reset_target = document.getElementById(text_id);
+  if(reset_target.value == ''){
+      alert('すでにリセットされています。');
+  }else{
+      reset_target.value = '';
+  };
+}
+//画像ダウンロード（ローカルではうまく機能しない）
+function saveCanvas(canvas_id)
+{
+	var canvas = document.getElementById(canvas_id);
+	var uri = canvas.toDataURL('image/jpeg', 0.85);
+	if (canvas.msToBlob) { //IE対応
+		var blob = toBlob(uri);
+		window.navigator.msSaveBlob(blob, 'download.jpg');
+	} else {
+		//アンカータグを作成
+		var a = document.createElement('a');
+		a.href = uri;
+    a.download = 'download.jpg';
+    //クリックイベントを発生させる
+		a.click();
+  }
+  
+}
